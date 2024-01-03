4b79c9a4f4d0126a5f7549e36ae3e4e485ca330a
Vinod Jatav <jatavvinod1997@gmail.com>
2024-01-04 00:50:53 +0530

    Adding tooltip on button hover.

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -1,16 +1,13 @@
 import AddIcon from "@mui/icons-material/Add";
 import ExploreIcon from "@mui/icons-material/Explore";
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import * as React from "react";
 import { DiscordIcon } from "../theme/Icons/Icons";
 
-const btnStyle = {
-  width: "48px",
-  height: "48px",
-  minWidth: "0px",
+const btnSxStyle = {
   color: "#50A361",
   borderRadius: "24px",
   background: "#313338",
@@ -20,9 +17,34 @@ const btnStyle = {
     background: "#50A361",
   },
 };
+const btnStyle = {
+  width: "48px",
+  height: "48px",
+  minWidth: "0px",
+};
 const iconSize = {
   width: "24px",
   height: "24px",
+};
+const tooltipComponentProps = {
+  tooltip: {
+    sx: {
+      bgcolor: '#000',
+      '& .MuiTooltip-arrow': {
+        color: '#000',
+      },
+    },
+  },
+};
+const tooltipPopperProps = {
+  modifiers: [
+    {
+      name: "offset",
+      options: {
+        offset: [0, 5],
+      },
+    },
+  ],
 };
 
 const pages = ["VJ"];
@@ -47,40 +69,68 @@ const ResponsiveAppBar = () => {
       <Box
         display="flex"
         flexDirection="column"
-        sx={{ flexGrow: 1, gap: "10px", marginTop: "10px" }}
+        sx={{ flexGrow: 1, gap: "10px", margin: "10px" }}
       >
-        <IconButton
-          aria-label="delete"
-          sx={{
-            width: "48px",
-            height: "48px",
-            color: "#5A65EA",
-            borderRadius: "24px",
-            background: "#313338",
-            "&:hover": {
-              borderRadius: "12px",
-              color: "#FFF",
-              background: "#5A65EA",
-            },
-          }}
-        >
-          <DiscordIcon />
-        </IconButton>
-        {pages.map((page) => (
-          <Button
-            key={page}
-            onClick={handleCloseNavMenu}
-            sx={btnStyle}
+        <Tooltip
+          title={<Typography>Direct Messages</Typography>}
+          placement="right"
+          arrow
+          componentsProps={tooltipComponentProps}
+          PopperProps={tooltipPopperProps}>
+          <IconButton
+            aria-label="Direct Messages"
+            style={btnStyle}
+            sx={{
+              color: "#5A65EA",
+              borderRadius: "24px",
+              background: "#313338",
+              "&:hover": {
+                borderRadius: "12px",
+                color: "#FFF",
+                background: "#5A65EA",
+              },
+            }}
           >
-            {page}
-          </Button>
+            <DiscordIcon />
+          </IconButton>
+        </Tooltip>
+        {pages.map((page) => (
+          <Tooltip
+            title={<Typography>VJ</Typography>}
+            placement="right"
+            arrow
+            componentsProps={tooltipComponentProps}
+            PopperProps={tooltipPopperProps}>
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              style={btnStyle}
+              sx={btnSxStyle}
+            >
+              {page}
+            </Button>
+          </Tooltip>
         ))}
-        <IconButton aria-label="delete" sx={btnStyle}>
-          <AddIcon sx={iconSize} />
-        </IconButton>
-        <IconButton aria-label="delete" sx={btnStyle}>
-          <ExploreIcon sx={iconSize} />
-        </IconButton>
+        <Tooltip
+          title={<Typography>Add a Server</Typography>}
+          placement="right"
+          arrow
+          componentsProps={tooltipComponentProps}
+          PopperProps={tooltipPopperProps}>
+          <IconButton aria-label="Add a Server" style={btnStyle} sx={btnSxStyle}>
+            <AddIcon sx={iconSize} />
+          </IconButton>
+        </Tooltip>
+        <Tooltip
+          title={<Typography>Explore Discoverable Servers</Typography>}
+          placement="right"
+          arrow
+          componentsProps={tooltipComponentProps}
+          PopperProps={tooltipPopperProps}>
+          <IconButton aria-label="Explore Discoverable Servers" style={btnStyle} sx={btnSxStyle}>
+            <ExploreIcon sx={iconSize} />
+          </IconButton>
+        </Tooltip>
       </Box>
     </AppBar>
   );
